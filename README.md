46567104f09a9604f4c9bd85da54ab929331fb52
ZainShabbir <zainansari08@gmail.com>
2020-09-07 13:27:58 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Personal Blog With VUE.js
+
+Personal Blog website build with VUE.js
+
+## Objectives
+
+1. Learn VUE.js
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&family=Roboto+Slab:wght@700&display=swap");
+:root {
+  --black-text: #33353b;
+  --purple: #5444da;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  width: 100%;
+  height: 100%;
+  background-color: #e8eaee;
+  margin-top: 70px;
+}
+</style>
