4baa9d27823f28d83656dce9710d4d316743c9bb
ZainShabbir <zainansari08@gmail.com>
2020-09-09 13:29:58 +0500

    add Header component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Navbar />
+    <Header />
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Header from "./components/Header.vue";
 
 export default {
   name: "App",
   components: {
     Navbar,
+    Header,
   },
 };
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header></header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data: {},
+};
+</script>
+
+<style scoped>
+header {
+  width: 100%;
+  height: 400px;
+}
+</style>
