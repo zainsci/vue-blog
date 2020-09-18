8b859e71cf14dd69a1704bdbb20ecaa79884b5ca
ZainShabbir <zainansari08@gmail.com>
2020-09-18 19:05:28 +0500

    Add SignUp Form and Label

---
+++ src/components/Forms/Label.vue
@@ -0,0 +1,17 @@
+<template>
+  <label>{{ label }}</label>
+</template>
+
+<script>
+export default {
+  name: "Label",
+  props: { label: String },
+};
+</script>
+
+<style scoped>
+label {
+  display: inline-block;
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="form-container">
+    <div class="signup-form">
+      <form action="/" method="POST">
+        <Label label="Email or Username" :for="'signup-username'" />
+        <Input type="text" name="signup-username" id="signup-username" />
+        <Label label="Password" :for="'signup-password'" />
+        <Input type="password" name="signup-password" id="signup-password" />
+        <Label label="Confirm Password" :for="'confirm-password'" />
+        <Input type="password" name="confirm-password" id="confirm-password" />
+        <Button btnText="Log In" />
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import Label from "./Forms/Label.vue";
+import Input from "./Forms/Input.vue";
+import Button from "./Components/Button.vue";
+
+export default {
+  name: "SignUpForm",
+  props: {},
+  components: {
+    Label,
+    Input,
+    Button,
+  },
+};
+</script>
+
+<style scoped>
+.form-container {
+  width: 100%;
+  height: calc(100vh - 70px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.signup-form {
+  width: 100%;
+  max-width: 400px;
+  padding: 32px;
+  background-color: var(--white);
+  border: var(--primary-border);
+  border-radius: var(--primary-radius);
+}
+</style>
