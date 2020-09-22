dfb4e491eb96fc573c077eea8405cdfd31ff5ef6
zainsci <zainansari08@gmail.com>
2020-09-22 20:23:20 +0500

    Add TagCloud Component

---
+++ src/components/TagCloud.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="tags">
+    <ul>
+      <li>
+        <a href="#">VueJs</a>
+      </li>
+      <li>
+        <a href="#">React</a>
+      </li>
+      <li>
+        <a href="#">Angular</a>
+      </li>
+      <li>
+        <a href="#">Moment</a>
+      </li>
+      <li>
+        <a href="#">Gulp</a>
+      </li>
+      <li>
+        <a href="#">Normalize</a>
+      </li>
+      <li>
+        <a href="#">Bootstrap</a>
+      </li>
+      <li>
+        <a href="#">Django</a>
+      </li>
+      <li>
+        <a href="#">Flask</a>
+      </li>
+      <li>
+        <a href="#">Python</a>
+      </li>
+      <li>
+        <a href="#">C</a>
+      </li>
+      <li>
+        <a href="#">C++</a>
+      </li>
+      <li>
+        <a href="#">Java</a>
+      </li>
+      <li>
+        <a href="#">JavaScript</a>
+      </li>
+      <li>
+        <a href="#">Haskel</a>
+      </li>
+      <li>
+        <a href="#">PHP</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TagCloud",
+};
+</script>
+
+<style scoped>
+</style>
