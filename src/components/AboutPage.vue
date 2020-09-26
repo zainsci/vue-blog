cf1ce71c17c870ad7fdade2cf99f408c2c4bb2ef
zainsci <zainansari08@gmail.com>
2020-09-26 17:52:26 +0500

    Add Styling to AboutPage

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,22 +1,27 @@
 <template>
-  <container>
-    <div class="about-img">
-      <img
-        src="https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=2500"
-        alt="Mountains"
-      />
-    </div>
-    <div class="about">
-      <h1>
-        About Us.
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto suscipit
-        labore mollitia nam cum officiis.
-      </p>
-      <Button />
-    </div>
-  </container>
+  <div
+    class="about"
+    style="background-image: url('https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=2500')"
+  >
+    <container>
+      <div class="about-text">
+        <h1>
+          About Us.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+          suscipit labore mollitia nam cum officiis. Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Vel ab, hic animi odit consequuntur
+          impedit, autem dolores quam, ipsum reiciendis laborum tempore dolor
+          eius minus aspernatur consectetur tempora molestias. Rem ut blanditiis
+          temporibus impedit porro perferendis soluta asperiores debitis aut
+          quaerat? Expedita doloribus debitis quae quisquam vitae ducimus?
+          Placeat, laboriosam.
+        </p>
+        <router-link to="/contact"><Button btnText="Contact"/></router-link>
+      </div>
+    </container>
+  </div>
 </template>
 
 <script>
@@ -31,4 +36,29 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.about {
+  width: 100%;
+  height: calc(100vh - 70px);
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  padding: 60px 0;
+}
+.about-text {
+  width: 50%;
+  min-width: 500px;
+}
+h1 {
+  font-weight: 600;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  border-bottom: 5px solid var(--primary);
+  width: fit-content;
+}
+p {
+  margin-bottom: 40px;
+  line-height: 1.5;
+  font-weight: 600;
+}
+</style>
