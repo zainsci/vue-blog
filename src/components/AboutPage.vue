b6da370c53976c1662a1a6051f97d85192c93a86
zainsci <zainansari08@gmail.com>
2020-09-26 17:33:58 +0500

    Add About Page Component

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <container>
+    <div class="about-img">
+      <img
+        src="https://images.unsplash.com/photo-1500534314209-a25ddb2bd429?w=2500"
+        alt="Mountains"
+      />
+    </div>
+    <div class="about">
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto suscipit
+        labore mollitia nam cum officiis.
+      </h1>
+      <Button />
+    </div>
+  </container>
+</template>
+
+<script>
+import container from "./Utils/Container.vue";
+import Button from "./Components/Button.vue";
+export default {
+  name: "about-page",
+  components: {
+    container,
+    Button,
+  },
+};
+</script>
+
+<style scoped></style>
