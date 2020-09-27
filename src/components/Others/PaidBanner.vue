6c1f992d19bbc392ff1dff4bdedbddd951050d30
zainsci <zainansari08@gmail.com>
2020-09-27 20:23:52 +0500

    Add Paid Banner

---
+++ src/components/Others/PaidBanner.vue
@@ -0,0 +1,68 @@
+<template>
+  <conatiner>
+    <div class="banner">
+      <div class="banner-img">
+        <img
+          src="https://images.unsplash.com/photo-1601126217456-1744cdad7211?w=1050"
+          alt="Food Table"
+        />
+      </div>
+      <div class="banner-text">
+        <a href="#">
+          <h4>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
+            repellat repellendus repudiandae quibusdam id, molestiae pariatur
+            recusandae necessitatibus excepturi in nam quae sapiente, expedita
+            quis?
+          </h4>
+        </a>
+        <small>Paid For By Lorem Ipsum</small>
+      </div>
+    </div>
+  </conatiner>
+</template>
+
+<script>
+import Conatiner from "../Utils/Container.vue";
+export default {
+  name: "paid-banner",
+  components: {
+    Conatiner,
+  },
+};
+</script>
+
+<style scoped>
+a {
+  text-decoration: none;
+  color: var(--text);
+}
+a:hover h4 {
+  color: var(--primary) !important;
+}
+.banner {
+  max-width: 700px;
+  min-height: 200px;
+  max-height: 250px;
+  background: rgba(0, 0, 0, 0.05);
+  color: var(--text);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 0;
+}
+.banner-img {
+  width: 350px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+}
+.banner-text {
+  width: 50%;
+  padding: 20px;
+  min-height: inherit;
+  line-height: 1.5;
+}
+</style>
--- src/components/Pages/HomePage.vue
+++ src/components/Pages/HomePage.vue
@@ -6,6 +6,7 @@
       bannerImg="https://images.unsplash.com/photo-1599904182194-d519a3268229?w=1229"
     />
     <featured-articles />
+    <paid-banner />
   </main>
 </template>
 
@@ -13,6 +14,7 @@
 import HeadPosts from "../Posts/HeadPosts.vue";
 import AdBanner from "../Others/AdBanner.vue";
 import FeaturedArticles from "../Pages/FeaturedArticles.vue";
+import PaidBanner from "../Others/PaidBanner.vue";
 
 export default {
   name: "home-page",
@@ -21,6 +23,7 @@ export default {
     HeadPosts,
     AdBanner,
     FeaturedArticles,
+    PaidBanner,
   },
 };
 </script>
